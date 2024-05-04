01c07c937adb9d1a42faf4486331a2a381b894fa
IanPedroBorges <ianhudback@gmail.com>
2024-05-04 07:27:34 -0300

    feat: add category products page

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,11 +1,23 @@
+"use client";
+
 import Image from "next/image";
 import { Button } from "./ui/button";
 import { MenuIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 const Header = () => {
+  const router = useRouter();
   return (
     <div className="flex justify-between px-5 pt-6">
-      <Image src="/logo.png" alt="FSW Foods" height={30} width={100} />
+      <div className="relative h-[30px] w-[100px]">
+        <Image
+          src="/logo.png"
+          alt="FSW Foods"
+          fill
+          className="object-cover"
+          onClick={() => router.push("/")}
+        />
+      </div>
       <Button
         size="icon"
         variant="outline"
